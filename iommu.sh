368aa697031b97295be2aa96c30b5da069019c60
JoonaHa <32388752+JoonaHa@users.noreply.github.com>
2021-03-17 18:39:43 +0200

    Update iommu.sh

--- iommu.sh
+++ iommu.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Source: 
+# Source: https://wiki.archlinux.org/index.php/PCI_passthrough_via_OVMF#Enabling_IOMMU
 for d in /sys/kernel/iommu_groups/*/devices/*; do
   n=${d#*/iommu_groups/*}; n=${n%%/*}
   printf 'IOMMU Group %s ' "$n"
